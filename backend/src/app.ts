67af39dc548c4248d5438d9405e2ad8bdc2ff05d
Racyn Ibaya <racyn@network-device.home>
2024-03-22 11:04:15 -0400

    add: Add 3rd party logger morgan
    This will loginformation of every http request method that is sent to the server.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import userRouter from './routes/userRouter';
 import petRouter from './routes/petRouter';
@@ -8,6 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from the server');
