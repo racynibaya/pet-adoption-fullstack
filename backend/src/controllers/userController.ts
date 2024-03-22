dda7f743e4e217362f1ea52043accce87ed59230
Racyn Ibaya <racyn@network-device.home>
2024-03-22 15:50:26 -0400

    feature: Add function to delete or get user
    These two functions are using the user id to find the specific user to delete or get

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -24,3 +24,43 @@ export const getAllUser = async (req: Request, res: Response) => {
     },
   });
 };
+
+export const getUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const user = await User.findById({ _id: id });
+
+  if (!user) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No existing user',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Successfully get the user',
+    user,
+  });
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const user = await User.findById({ id });
+
+  if (!user) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No existing user',
+    });
+  }
+
+  const deleteUser = await User.findByIdAndDelete({ id });
+
+  res.status(204).json({
+    status: 'success',
+    message: 'User sucessfully deleted',
+    deleteUser,
+  });
+};
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,9 +1,18 @@
-import express, { Request, Response } from 'express';
-import User from '../models/user';
-import { createUser, getAllUser } from '../controllers/userController';
+import express from 'express';
+
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  getUser,
+} from '../controllers/userController';
 
 const router = express.Router();
 
 router.route('/').post(createUser).get(getAllUser);
+
+router.route('/:id').delete(deleteUser).get(getUser);
+
+router.route('/login').post();
 
 export default router;
